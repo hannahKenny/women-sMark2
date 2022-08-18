1884d3115826302d3699703e321c264af62aec67
Hannah Kenny <hannah@bostonkennys.com>
2022-08-18 11:43:42 -0400

    added the buttons for the correct screens to start work on the score keeper

--- women'sMark2/ViewController.swift
+++ women'sMark2/ViewController.swift
@@ -14,7 +14,27 @@ class ViewController: UIViewController {
     
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func q1correct(_ sender: UIButton) {
+        //if this button gets pressed, add 1 to variable which counts the score
+    }
+    
+    
+    @IBAction func q2Correct(_ sender: UIButton) {
+        //if this button gets pressed, add 1 to variable which counts the score
+    }
+    
+    
+    @IBAction func q3Correct(_ sender: UIButton) {
+        //if this button gets pressed, add 1 to variable which counts the score
+    }
+    
+    
+    @IBAction func q4Correct(_ sender: UIButton) {
+        //if this button gets pressed, add 1 to variable which counts the score
+    }
+    
 
 }
 
